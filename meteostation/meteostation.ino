8417de05f3dd3fdda231eb9f17a122f5902fc0e0
Vadym <vadosdv00000@gmail.com>
2019-05-01 23:26:29 +0300

    connect DHT-11 sensor

--- meteostation/meteostation.ino
+++ meteostation/meteostation.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "DHT.h"
+#define DHTPIN 2     // к какому пину будет подключен вывод Data
 
+//выбор используемого датчика
+#define DHTTYPE DHT11   // DHT 11 
+
+//инициализация датчика
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600); 
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // чтение температуры и влажности займет примерно 250 миллисекунд
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
+  // проверяем правильные ли данные получили
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Error reading from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
 }
